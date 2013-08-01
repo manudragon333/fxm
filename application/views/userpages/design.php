dda739e51528b179f677c700b958f181f50d5e3c
vikaspaturi <lovepixelperfect@gmail.com>
2013-08-02 02:30:31 +0530

    -added files

--- application/views/userpages/design.php
+++ application/views/userpages/design.php
@@ -1,79 +1,98 @@
-    <?php $this->load->view('common/userpages/main_header'); ?>
-
-    <script type="text/javascript" src="<?php echo base_url(); ?>public/js/ckeditor/ckeditor.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>public/js/ckeditor/adapters/jquery.js"></script>
-    <script type="text/javascript">
-        var site_url = '<?php echo site_url(); ?>'
-        $(document).ready(function(){
-            $.ajaxSetup({
-                jsonp: null,
-                jsonpCallback: null
-            });
-
-        });
-
-    </script>
-
-    <style type="text/css">
-        div.panel select {
-            width: 180px !important;
-        }
-    </style>
-
-</head>
-
-
-<body class="app">
-    <?php $this->load->view('common/userpages/leftnav'); ?>
-
-    <div class="right_content">
-
-        <?php $this->load->view('common/userpages/menu_header'); ?>
-
-
-        <section id="secondary_bar" class="section">
-
-            <div class="breadcrumbs_container">
-                <article class="breadcrumbs article">
-                    <img src="<?php echo base_url(); ?>misc/css/images/fav.png" class="fl bread_logo" />
-                    <div class="breadcrumb_divider"></div>
-                    <a class="current">Trade</a>
-                </article>
-                
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/reset.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/animate.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/css3-buttons.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/app.css" />
+    </head>
+    <body class="app">
+        <div id="wrap">
+            <div class="header animation_setting bounceInDown">
+                <div class="logo_inner p_a"></div>
+                <div class="p_a app_interaction">
+                    <a class="f_l chat first tf_animation">Chat</a>
+                    <a class="f_l logged_user tf_animation p_r">
+                        <span class="usr_ico icon p_a tf_animation"></span>Admin
+                    </a>
+                    <a class="f_l logout last">logout</a>
+                    <div class="c_b"></div>
+                </div>
             </div>
-        </section><!-- end of secondary bar -->
+            <div class="contentarea p_r">
+                <div class="leftNav p_f tf_animation animation_setting bounceInLeft">
+                    <ul class="nav_emt">
+                        <li><a href="" class="active">Home</a></li>
+                        <li><a href="">Student Data</a></li>
+                        <li><a href="">User Accounts</a></li>
+                        <li><a href="">College Updates</a></li>
+                        <li><a href="">Polls</a></li>
+                        <li><a href="" class="collapse">College Administration</a></li>
+                        <ul class="lft_sub_menu">
+                            <li>
+                               <a href=""> College management </a>
+                            </li>
+                            <li>
+                                <a href=""> Course management </a>
+                            </li>
+                            <li>
+                                <a href=""> Branch management </a>
+                            </li>
+                            <li>
+                                <a href=""> Semester management </a>
+                            </li>
+                            <li>
+                                <a href=""> Subject management </a>
+                            </li>
+                            <li>
+                                <a href=""> Period Cycle management </a>
+                            </li>
+                            <li>
+                                <a href=""> Attendance management </a>
+                            </li>
+                            <li>
+                                <a href=""> Teaching Years </a>
+                            </li>
+                            <li>
+                                <a href=""> Section management </a>
+                            </li>
+                            <li>
+                                <a href=""> Batch No management </a>
+                            </li>
+                            <li>
+                                <a href=""> Calendar Items </a>
+                            </li>
+                            <li>
+                               <a href=""> Academic Calendar </a>
+                            </li>
+                            <li>
+                               <a href=""> Send Message </a>
+                            </li>
+                        </ul>
+                        <li><a href="">Email</a></li>
+
+                    </ul>
+                </div>
+                <div class="rightNav">
+                    <div class="rightNav_head">Header</div>
+                    <div class="rightNav_cnt">
+                        
+                        
+
+                         <a class="button grey">Grey</a>
+                         <a class="button red">red</a>
+                         <a class="button blue">blue</a>
+                         <a class="button green">green</a>
+                         <a class="button black">black</a>
+                         <a class="button yellow">yellow</a>
+                         <a class="button purple">purple</a>
+                         <a class="button gblue">gblue</a>
 
 
-
-        <section id="main" class="column">
-
-
-            <div class="form_prp mar0">
-
-
-                <!-- START @@ CONTENT GOES HERE-->
-
-                CONTENT GOES HERE
-
-                <!-- END @@ CONTENT GOES HERE-->
-
-
+                    </div>
+                </div>
             </div>
-        </section>
-
-        <!--Start@@code for the Modal Window-->
-
-        <div id="addpayment" class="m_w">
-
-        </div>
-
-
-        <!--End@@code for the Modal Window-->
-
-        <!-- End of RTE Files -->
-
-    </div>
-
-</body>
-
+        </div>    
+        <div id="footer"></div>    
+    </body>
 </html>
