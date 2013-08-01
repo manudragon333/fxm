c44358892252cc4a413c81bd5179d5f2088fc89d
vikaspaturi <lovepixelperfect@gmail.com>
2013-08-02 02:39:50 +0530

    -added new files

--- application/views/userpages/design.php
+++ application/views/userpages/design.php
@@ -5,6 +5,8 @@
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/animate.css" />
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/css3-buttons.css" />
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/css/app.css" />
+        <script type="text/javascript" src="<?php echo base_url(); ?>public/js/jquery-1.8.2.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>public/js/cssua.min.js"></script>
     </head>
     <body class="app">
         <div id="wrap">
